044567caf9a8c620a03016b296a6d4f8bd741cab
parezban <parezban@gmail.com>
2020-06-22 23:56:19 +0430

    add some basic function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-export const Balidator = (name: string) =>             `Hello  ${name}`;
+type BalidatorType = {
+    [key: string]: (v: any) => boolean;
+};
+
+const Balidator: BalidatorType = {
+    isDefined: (arg: any) => arg !== undefined && arg !== null,
+    isFunc: (arg: any) => typeof arg === "function",
+    isBoolean: (arg: any) => typeof arg === "boolean",
+    isObject: (arg: any) => arg === Object(arg),
+    isNumber: (arg: any) => typeof arg === "number",
+    isInteger: (arg: any) => Balidator.isNumber(arg) && arg % 1 === 0,
+    isString: (arg: any) => typeof arg === "string",
+    isArray: (arg: any) => Array.isArray(arg),
+    isNull: (arg: any) => arg === null,
+    isPromise: (arg: any) => Balidator.isDefined(arg.then) && Balidator.isFunc(arg.then),
+
+}
