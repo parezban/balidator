e65fa242986895372e5e4b96ed0d6b9f2b148302
parezban <parezban@gmail.com>
2020-06-23 23:30:11 +0430

    add range valdiator

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
-type BalidatorType = {
+type BaseBalidatorType = {
     [key: string]: (v: any) => boolean;
 };
 
-const Balidator: BalidatorType = {
+type RangeBalidatorType = {
+    [key: string]: (v: any, v2: any, v3: any) => boolean;
+};
+
+const BaseBalidator: BaseBalidatorType = {
     isDefined: (arg: any) => arg !== undefined && arg !== null,
     isFunc: (arg: any) => typeof arg === "function",
     isBoolean: (arg: any) => typeof arg === "boolean",
     isObject: (arg: any) => arg === Object(arg),
     isNumber: (arg: any) => typeof arg === "number",
-    isInteger: (arg: any) => Balidator.isNumber(arg) && arg % 1 === 0,
+    isInteger: (arg: any) => BaseBalidator.isNumber(arg) && arg % 1 === 0,
     isString: (arg: any) => typeof arg === "string",
     isArray: (arg: any) => Array.isArray(arg),
     isNull: (arg: any) => arg === null,
-    isPromise: (arg: any) => Balidator.isDefined(arg.then) && Balidator.isFunc(arg.then),
+    isPromise: (arg: any) => BaseBalidator.isDefined(arg.then) && BaseBalidator.isFunc(arg.then),
+
+}
+const RangeBalidator: RangeBalidatorType = {
+    inRangeNumber: (value: number, min?: number, max?: number) => (min && max && min < value && value <= max) || (max && value <= max) || (min && min < value) || false,
+
 
 }
